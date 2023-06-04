41ea76590ed142cfd649885b341700f167981919
Fanon X Rogers <fanonxrogers@Fanons-MacBook-Pro.local>
2023-06-04 19:26:30 -0400

    get tests to compile and run. Tests are failing.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+pub struct KvStore {}
+
+impl KvStore {
+    // declare a new instance of the KvStore
+    pub fn new() -> KvStore {
+        unimplemented!()
+    }
+
+    pub fn set<'a>(&mut self, key: String, val: String) {
+        unimplemented!()
+    }
+
+    pub fn remove(&mut self, key: String) {
+        unimplemented!()
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        unimplemented!()
+    }
 }
